66e2ee4eec3f067e7ef09a089188ea6b54f18fc0
Lucas A Evaldt <lucasantonioevaldt@gmail.com>
2024-07-12 16:43:52 -0300

    dffdgdfgsfsdfs

--- 000hello-world/main.js
+++ 000hello-world/main.js
@@ -1,1 +1,19 @@
 alert('Hello, World!');
+
+function oQueEuSouDoPadre() {
+    let meuPai = {
+        filho: 'Eu'
+    };
+
+    let eu = {
+        paiDoPadre: 'Eu'
+    };
+
+    if (meuPai.filho === 'Eu' && eu.paiDoPadre === 'Eu') {
+        return 'filho';
+    } else {
+        return 'desconhecido';
+    }
+}
+
+alert(oQueEuSouDoPadre());
