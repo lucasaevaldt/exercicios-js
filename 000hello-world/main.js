00d2c7c8dd5f5924d9f90964ccf6c581e8ea67b2
lucasaevaldt <lucasantonioevaldt@gmail.com>
2024-10-02 22:41:14 +0000

    Exercício 01

--- 000hello-world/main.js
+++ 000hello-world/main.js
@@ -1,19 +1,3 @@
+console.log('Hello, World!');
+
 alert('Hello, World!');
-
-function oQueEuSouDoPadre() {
-    let meuPai = {
-        filho: 'Eu'
-    };
-
-    let eu = {
-        paiDoPadre: 'Eu'
-    };
-
-    if (meuPai.filho === 'Eu' && eu.paiDoPadre === 'Eu') {
-        return 'filho';
-    } else {
-        return 'desconhecido';
-    }
-}
-
-alert(oQueEuSouDoPadre());
