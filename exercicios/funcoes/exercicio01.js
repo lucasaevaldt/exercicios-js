78262f84d5580d1ec45b59608a7386c3dcbd38b4
lucasaevaldt <lucasantonioevaldt@gmail.com>
2024-11-15 16:19:36 -0300

    funcões exercicios inicio

---
+++ exercicios/funcoes/exercicio01.js
@@ -0,0 +1,9 @@
+//Exercício 01
+
+//Crie uma função chamada saudacao que receba um nome como parâmetro e retorne a mensagem "Olá, [nome]!".
+function saudacao(nome) {
+    return "Olá, " + nome + "!";
+}
+
+console.log(saudacao("Lucas"));
+//Olá, Lucas!
---
+++ exercicios/funcoes/exercicio02.js
@@ -0,0 +1,9 @@
+//Exercício 02
+
+//Crie uma função chamada quadrado que receba um número e retorne o quadrado desse número.
+function quadrado(num1) {
+    return num1 * num1;
+}
+
+console.log(quadrado(9));
+//81
