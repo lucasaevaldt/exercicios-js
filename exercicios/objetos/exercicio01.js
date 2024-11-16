4c24db6606eb1e7c483679e0f5d9154f4d251338
lucasaevaldt <lucasantonioevaldt@gmail.com>
2024-11-16 16:07:58 -0300

    objetos inicio

---
+++ exercicios/objetos/exercicio01.js
@@ -0,0 +1,12 @@
+//Exercício 01
+
+//Crie um objeto chamado carro com as propriedades: marca, modelo, e ano. Exiba cada uma delas no console.
+const carro = {
+    marca: "Chevrolet",
+    modelo: "Onix",
+    ano: 2022 
+}
+
+console.log(carro.marca); //Chevrolet
+console.log(carro.modelo); //Onix
+console.log(carro.ano); //2022
---
+++ exercicios/objetos/exercicio02.js
@@ -0,0 +1,14 @@
+//Exercício 02
+
+//Adicione uma nova propriedade chamada cor ao objeto carro e exiba no console.
+let carro = {
+    marca: "Chevrolet",
+    modelo: "Onix",
+    ano: 2022 
+}
+
+carro.cor = "Branco";
+console.log(carro.marca); //Chevrolet
+console.log(carro.modelo); //Onix
+console.log(carro.ano); //2022
+console.log(carro.cor); //Branco
