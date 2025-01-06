855cff76ded04cc054600ed15bc53081d9f291c0
lucasaevaldt <lucasantonioevaldt@gmail.com>
2025-01-05 23:31:14 -0300

    05012025 01

---
+++ accordion/script.js
@@ -0,0 +1,9 @@
+const accordion = document.querySelectorAll('.js-accordion');
+
+accordion.forEach(acc => {
+    acc.addEventListener("click", () => {
+        let dad = acc.parentElement;
+
+        dad.classList.toggle('active');
+    })
+})
